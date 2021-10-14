6bc2c0eeb7c120ab41c6318de3dbcb96c1d7c246
RaulORios <raul.orios89@outlook.com>
2021-10-14 13:26:46 -0300

    Arquivos atualizados

---
+++ src/Exer_Fixacao_Heranca_Orient_Objeto/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exer_Fixacao_Heranca_Orient_Objeto.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return ($"{Name} $ {Price.ToString("F2",CultureInfo.InvariantCulture)}"); 
+        }
+
+    }
+}
